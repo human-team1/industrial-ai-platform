e830e4869d51c11635fc51fab0d31e2657706f18
hee-03 <oeo223@gmail.com>
2026-04-30 14:50:25 +0900

    refactor: 미사용 검사 DTO/UseCase 제거

--- backend-spring/src/main/java/com/example/factoryguard/application/port/in/inspection/CreateUploadInspectionUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.example.factoryguard.application.port.in.inspection;
-
-import com.example.factoryguard.application.dto.inspection.CreateUploadInspectionCommand;
-import com.example.factoryguard.application.dto.inspection.CreateUploadInspectionResult;
-
-public interface CreateUploadInspectionUseCase {
-
-    CreateUploadInspectionResult execute(CreateUploadInspectionCommand command);
-}
